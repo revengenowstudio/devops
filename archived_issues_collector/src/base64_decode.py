6423b40a88f88b82c45d701f8f5ca8c85d918d5e
shuiping233 <1944680304@qq.com>
2025-11-04 15:43:24 +0800

    cleanup: 移除了非geo2ip的内容

--- archived_issues_collector/src/base64_decode.py
+++
@@ -1,4 +0,0 @@
-import base64
-
-def base64_decode_str(data: str) -> str:
-    return base64.b64decode(data).decode('utf-8')
--- archived_issues_collector/src/data_source.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class DataSource(ABC):
-
-    @abstractmethod
-    def load(self,config: object) -> None:
-        pass
